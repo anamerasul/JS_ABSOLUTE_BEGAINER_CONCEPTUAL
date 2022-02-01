cf74989566b9c36ed8a7ac78b87729ac2219ce2c
anamerasul <anamerasul@gmail.com>
2022-02-01 20:31:29 +0600

    in

---
+++ index.js
@@ -0,0 +1,121 @@
+/* var glass="water";
+// b=555;
+// a=b;
+console.log(glass);
+
+var myName="Tonmoy";
+
+console.log(myName);
+
+
+console.log(myName) */
+
+// datatypes
+
+// 1.primitive datatypes
+// String
+
+var myName ="Tonmoy";
+console.log(myName);
+
+var firstName ="anam";
+
+var lastName="tonmoy";
+
+var fullName=firstName+" "+lastName;
+
+console.log(fullName);
+
+// number
+
+var age1 =30;
+
+var age2="40";
+
+console.log(typeof age2);
+
+
+var sum =age1+age2;
+
+
+console.log(sum);
+
+
+console.log(typeof sum);
+
+var sum =age1+parseInt(age2);
+
+console.log(sum);
+
+
+console.log(typeof sum);
+
+// bolean
+
+// 0=false
+// 1= true
+
+var number1 =60;
+
+var number2 =60;
+
+console.log(number1==number2);
+
+// undefined
+var rollNumber;
+console.log(rollNumber);
+
+console.log(typeof rollNumber);
+
+// null 
+var x= null;
+
+console.log(x);
+console.log(typeof x);
+
+
+// condition
+
+// if(condition){
+
+// }
+
+// else{
+
+// }
+
+// var number1=60;
+// var number2=50;
+
+// if(number1>number2){
+//                console.log(number1,"is biger");
+
+// }
+
+// else{
+//                console.log(number2, "is biger")
+
+// }
+
+var number= 11;
+
+if (number%2==0 && number!=0 ){
+               console.log(number,"even")
+}
+
+else if(number==0){
+               console.log(number, "unique");
+}
+
+else{
+               console.log(number, "odd");       
+}
+// 42.08
+
+
+
+
+
+
+
+
